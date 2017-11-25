3dec33b22ddf4b347e9ec79ae33821093b66ead2
cion94 <cristian.ion94@gmail.com>
2017-11-25 21:50:07 +0200

    index

--- ark/arkapp/urls.py
+++ ark/arkapp/urls.py
@@ -1,7 +1,6 @@
-from django.conf.urls import include, url
-from django.contrib import admin
+from django.conf.urls import url
+from . import views
 
 urlpatterns = [
-    url(r'^arkapp/', include('arkapp.urls')),
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index, name='index'),
 ]
